37fcac96007ea79eb696a12ebd09a33c25a65845
jh-rakib <52291033+jh-rakib@users.noreply.github.com>
2019-12-23 13:09:35 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+// jquery plugin
+
+$('#web').pieChart({
+	barColor: '#30bae7',
+	trackColor: '#dfe8ed',
+	lineWidth: 12,
+	size: 150,
+});
+
+$('#css').pieChart({
+	barColor: '#d74680',
+	trackColor: '#dfe8ed',
+	lineWidth: 12,
+	size: 150,
+});
+
+$('#gd').pieChart({
+	barColor: '#15c7a8',
+	trackColor: '#dfe8ed',
+	lineWidth: 12,
+	size: 150,
+});
+
+$('#ui').pieChart({
+	barColor: '#eb7d4b',
+	trackColor: '#dfe8ed',
+	lineWidth: 12,
+	size: 150,
+});
+
+
+
